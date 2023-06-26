c1a2c2b7a92cfb1d08ef07439f6d7524cc30c35e
Eric Junior <83984650+Eric20Junior@users.noreply.github.com>
2023-06-26 18:40:56 +0100

    Update settings.py

--- nearbyparish/settings.py
+++ nearbyparish/settings.py
@@ -185,11 +185,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Email configuration
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'eric20junior@gmail.com'
-EMAIL_HOST_PASSWORD = 'dynamic1997'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
